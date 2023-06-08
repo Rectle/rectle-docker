4ac1e31b2b651f700d6b929147ef3bb2cb2ebb7e
bartkalit <bartkalit@gmail.com>
2023-06-09 01:14:54 +0200

    add live output read

--- subman/src/project_env/environment.py
+++ subman/src/project_env/environment.py
@@ -6,7 +6,8 @@ import venv
 class Environment:
     def __init__(self):
         self.venv_dir = f'venv/'
-        self.activate_script = '/app/' + self.venv_dir + f'bin/activate'
+        self.bin_dir = '/app/' + self.venv_dir + f'bin/'
+        self.activate_script = self.bin_dir + 'activate'
         venv.create(self.venv_dir, with_pip=True, clear=False)
         
 
@@ -14,23 +15,35 @@ class Environment:
         try:
             subprocess.run(f'. {self.activate_script} && python -m pip install --upgrade pip', shell=True, check=True)
             subprocess.run(f'. {self.activate_script} && pip install -r src/requirements.txt', shell=True, check=True)
-            subprocess.run(f'. {self.activate_script} && pip install pipreqs', shell=True, check=True)
+
         except Exception as e:
             print("Env build failed")
             print(e)
 
-
     def run(self):
         try:
-            subprocess.run(f'. {self.activate_script} && python -u volume/project/main.py volume/project/', shell=True, check=True)
+            print("run started:")
+            process = subprocess.Popen([f'{self.bin_dir}python', '-u', 'volume/project/main.py', 'volume/project/'], stdout=subprocess.PIPE)
+            self.send_logs(process)
         except Exception as e:
             print("Run failed")
+            print(e)
+
+    def send_logs(self, process):
+        try:
+            while process.poll() is None:
+                output = process.stdout.readline()
+                if output:
+                    # TODO - add sending logs to the socket server
+                    print("LOGS: " + output.decode('ascii').strip())
+        except Exception as e:
+            print("Collecting output failed")
             print(e)
 
 
     def add_project_dependencies(self):
         try:
-            subprocess.run(f'. {self.activate_script} && pipreqs --savepath requirements.txt volume/project', shell=True, check=True)
+            subprocess.run(f'. {self.activate_script} && pipreqs --savepath requirements.txt volume', shell=True, check=True)
             subprocess.run(f'. {self.activate_script} && pip install -r requirements.txt', shell=True, check=True)
         except Exception as e:
             print("Failed while adding project dependencies")
